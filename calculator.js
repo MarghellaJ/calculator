fe05d17156f88b42a7021efedfb1bd12eeaddf11
Jim <thatdeveloperjim@gmail.com>
2025-01-20 11:13:18 -0500

    basic functions
    built functions for add, subtract, divide, multiply

---
+++ calculator.js
@@ -0,0 +1,31 @@
+// function to add numbers
+const add = function(n1, n2) {
+   return n1 + n2;
+};
+console.log(add(5, 7));
+
+// function to subtract numbers
+const subtract = function(n1, n2) {
+   return n1 - n2;
+};
+console.log(subtract(24, 8));
+
+
+// function to divide numbers
+const divide = function(n1, n2) {
+   return n1 / n2;
+};
+console.log(divide(15, 3));
+
+
+// function to multiply numbers
+const multiply = function(n1, n2) {
+   return n1 * n2;
+};
+console.log(multiply(5, 5));
+
+
+// function to clear screen/memory
+
+
+// function for button click eventListener
